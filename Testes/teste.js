316bdf8141a9b119f264dd5d587fb0bb5887a7bf
Aurino Junior <aurino_aj@hotmail.com>
2018-03-20 18:32:34 -0300

    removendo lixo

--- Testes/teste.js
+++ Testes/teste.js
@@ -56,30 +56,3 @@ QUnit.test("Sexto teste",
 		assert.ok(saidaEsperada == saida, "Passou com valor fora do intervalo");
 	}
 );
-
-/*var Teste = {					
-	validaPontos:function(number){
-		var aux = number, i, j;
-		var pontinhos = "";
-
-		if(isNaN(number)){
-			return "Voce nao passou um numero !!!"
-		}else if(number > 100 || number <=0 ){							
-			return "vai com calma ai !!!";
-		}else {
-			for (i = 0; i<number; i++) {							
-				
-				for(j=aux; j<number;j++){
-					pontinhos+="&nbsp&nbsp";
-				}
-
-				for (j=0;j<aux;j++){						
-					pontinhos+="*";
-				}
-				pontinhos+="<br>";
-				aux-=1;
-			}
-			return pontinhos;
-		}
-	}
-}*/
