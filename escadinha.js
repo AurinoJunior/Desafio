7ee67ccddf17e461e39cd56fe873339e7b2b4f14
Aurino Junior <aurino_aj@hotmail.com>
2018-03-21 16:46:39 -0300

    funcao converter para console

--- escadinha.js
+++ escadinha.js
@@ -1,34 +1,45 @@
-var Teste = {					
+var TesteHtml = {	
+
 	validaPontos:function(number){
 		var aux = number, i, j;
 		var pontinhos = "";
 
-		if(isNaN(number)){
-			return "Voce nao passou um numero !!!"
-		}else if(number > 100 || number <=0 ){							
-			return "vai com calma ai !!!";
-		}else {
-			for (i = 0; i<number; i++) {							
-				
-				for(j=aux; j<number;j++){
-					pontinhos+="&nbsp&nbsp";
-				}
-
-				for (j=0;j<aux;j++){						
-					pontinhos+="*";
-				}
-				pontinhos+="<br>";
-				aux-=1;
-			}
-			return pontinhos;
+		if (isNaN(number)) {
+			return "Voce nao passou um numero !!!";
 		}
+		if (number > 100 || number <=0) {
+			return "vai com calma ai !!!";
+		}
+
+	    for (i = 0; i<number; i++) {
+			
+			for(j=aux; j<number;j++){
+				pontinhos+="&nbsp&nbsp";
+			}
+			for (j=0;j<aux;j++){
+				pontinhos+="*";
+			}
+
+			pontinhos+="</br>"
+			aux-=1;
+		}
+		return pontinhos;
 	}
 }
 
-//Descomente para sair do modo teste
+var TesteConsole = {
+	converte: function(string){
+		for(var i=0; i<string.length; i++){
+			string=string.replace("&nbsp&nbsp"," ");
+			string=string.replace("</br>","\n");
+		}
+		return string;
+	}
+}
 
-/*var x = parseInt(prompt("Digite o numero"));
-var resultado = Teste.validaPontos(x);
-document.write(resultado);*/
+var x = parseInt(prompt("Digite o numero"));
+var resultado = TesteHtml.validaPontos(x);
+document.write(resultado);
 
-
+resultado = TesteConsole.converte(resultado);
+console.log(resultado);
