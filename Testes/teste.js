a9492ddf2ec9fab51aabdaf94f74f8f12dcc2be6
Aurino Junior <aurino_aj@hotmail.com>
2018-03-21 16:47:16 -0300

    Testes novos adicionados

--- Testes/teste.js
+++ Testes/teste.js
@@ -1,26 +1,26 @@
 QUnit.test("Primeiro teste", 
 	function(assert) {
 		var entrada = 3;
-		var saida = Teste.validaPontos(entrada);
-		var saidaEsperada = 48;
-
-   		assert.ok( saidaEsperada == saida.length, "Passou com numero inteiro!" );
-});
+		var saida = TesteConsole.converte(TesteHtml.validaPontos(entrada));
+   		var saidaEsperada = "***\n **\n  *\n";
+   		assert.equal(saidaEsperada, saida, "Gerou os pontos corretamente");
+	}
+);
 
 QUnit.test("Segundo teste",
 	function(assert){
 		var entrada = 4;
-		var saida = Teste.validaPontos(entrada);
-		var saidaEsperada = 86;
+		var saida = TesteHtml.validaPontos(entrada);
+		var saidaEsperada = "****</br>&nbsp&nbsp***</br>&nbsp&nbsp&nbsp&nbsp**</br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp*</br>";
 		
-		assert.ok(saidaEsperada == saida.length,"Passou com numero inteiro!");
+		assert.equal(saida, saidaEsperada, "Testado corretamente");
 	}
 );
 
 QUnit.test("Terceiro teste",
 	function(assert){
 		var entrada = "sdadhasu";
-		var saida = Teste.validaPontos(entrada);
+		var saida = TesteHtml.validaPontos(entrada);
 		var saidaEsperada = "Voce nao passou um numero !!!";
 
 		assert.ok(saidaEsperada == saida, "Passou com uma string");
@@ -30,7 +30,7 @@ QUnit.test("Terceiro teste",
 QUnit.test("Quarto teste",
 	function(assert){
 		var entrada = 15000;
-		var saida = Teste.validaPontos(entrada);
+		var saida = TesteHtml.validaPontos(entrada);
 		var saidaEsperada = "vai com calma ai !!!";
 
 		assert.ok(saidaEsperada == saida, "Passou com valor fora do intervalo");
@@ -40,7 +40,7 @@ QUnit.test("Quarto teste",
 QUnit.test("Quinto teste",
 	function(assert){
 		var entrada = -5;
-		var saida = Teste.validaPontos(entrada);
+		var saida = TesteHtml.validaPontos(entrada);
 		var saidaEsperada = "vai com calma ai !!!";
 
 		assert.ok(saidaEsperada == saida, "Passou com valor fora do intervalo");
@@ -50,9 +50,10 @@ QUnit.test("Quinto teste",
 QUnit.test("Sexto teste",
 	function(assert){
 		var entrada = 0;
-		var saida = Teste.validaPontos(entrada);
+		var saida = TesteHtml.validaPontos(entrada);
 		var saidaEsperada = "vai com calma ai !!!";
 
 		assert.ok(saidaEsperada == saida, "Passou com valor fora do intervalo");
 	}
 );
+
